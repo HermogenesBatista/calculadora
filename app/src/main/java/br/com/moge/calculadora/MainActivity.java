3b2000d9bd92341230589b1383a69a8534051573
Hermogenes Batista da Silva Filho <exemogenes@gmail.com>
2015-01-23 21:48:21 -0300

    Atualização
    tentativa de debugar o código.

--- app/src/main/java/br/com/moge/calculadora/MainActivity.java
+++ app/src/main/java/br/com/moge/calculadora/MainActivity.java
@@ -2,16 +2,17 @@ package br.com.moge.calculadora;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
     private TextView mostraCalc;
-
+    private String[] numeros;
+    private String[] operadores;
+    private String exibeCalc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,22 +20,78 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         mostraCalc = (TextView) findViewById(R.id.visualizar);
+        numeros = new String[10];
+        operadores = new String[10];
     }
 
 
     public void setNumber(View v){
 
-        Button b = (Button)v;
+        Button b = (Button) v;
         String buttonText = b.getText().toString();
 
+        for(int i =0; i<10; i++){
+
+            if(numeros[i] == null){
+                numeros[i] = buttonText;
+                exibeCalc = numeros[i];
+                break;
+            }else{
+
+                if(operadores[i] == null){
+                    numeros[i] +=buttonText;
+                    exibeCalc = numeros[i];
+                    break;
+                }else{
+
+                    if(operadores[i+1] == null){
+
+                        numeros[i+1] +=buttonText;
+                        exibeCalc += numeros[i+1]+operadores[1];
+                    }
+
+                }
+
+            }
+
+        }
+
+        mostraCalc.setText(exibeCalc);
 
     }
-    public void onClick(View v) {
+
+    public void setOperador(View v) {
         // 1) Possibly check for instance of first
 
         Button b = (Button)v;
         String buttonText = b.getText().toString();
 
-        mostraCalc.setText(buttonText);
+        for(int i =0; i<10; i++){
+
+            if(numeros[i] == null){
+                Toast.makeText(this,"numero vazio"+i,Toast.LENGTH_SHORT).show();
+                break;
+            }else{
+                Toast.makeText(this,"numero não vazio"+i, Toast.LENGTH_SHORT).show();
+                if(operadores[i] == null){
+                    Toast.makeText(this,"operador vazio"+i, Toast.LENGTH_SHORT).show();
+                    operadores[i] = buttonText;
+                    exibeCalc = numeros[i]+operadores[i];
+                    break;
+                }else{
+                    Toast.makeText(this,"operador não vazio"+i, Toast.LENGTH_SHORT).show();
+                    if(numeros[i+1] == null){
+                        operadores[i+1] +=buttonText;
+                        exibeCalc += numeros[i+1]+operadores[1];
+
+                    }
+
+                }
+
+            }
+
+        }
+
+        mostraCalc.setText(exibeCalc);
     }
 }
